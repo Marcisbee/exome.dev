1cc0bdf82a772c78946be23842ef8a73effd5600
Marcis Bergmanis <marcisbergmanis@gmail.com>
2024-02-01 20:47:31 +0200

    Update vocs.config.ts

--- vocs.config.ts
+++ vocs.config.ts
@@ -129,6 +129,10 @@ export default defineConfig({
           text: 'Testing',
           link: '/guides/testing',
         },
+        {
+          text: 'Devtools',
+          link: '/guides/devtools',
+        },
       ],
     },
 
